318939b0642c6234f72a63e8d4776672fa6a8ed7
rejohnbd <rejohn.cse@gmail.com>
2023-07-02 18:49:39 +0600

    Add Basic Dashboard Component

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard.index');
+        $countRoles = Role::count();
+        $countUser = User::count();
+        $employeeUser = User::where('role_id', 3)->count();
+        $clientUser = User::where('role_id', 2)->count();
+        return view('admin.dashboard.index', compact('countRoles', 'countUser', 'employeeUser', 'clientUser'));
     }
 }
